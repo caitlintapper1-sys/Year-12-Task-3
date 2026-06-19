f9e89e43cbdeefda955842628b0414ce196917a5
Caitlin T <catlin.tapper@education.nsw.gov.au>
2026-06-20 03:06:40 +1000

    19/06

--- app.py
+++ app.py
@@ -7,12 +7,22 @@ app = Flask(__name__)
 #route for test homepage
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    #makes it load the base.html template
+    #makes it load the main.html template
     return render_template('main.html')
 
+#route for menu, opens on 'game' tab by default
 @app.route("/menu", methods=['GET', 'POST'])
 def menu():
     return render_template('menu_game.html')
+#route for clues tab in menu
+@app.route("/menu_clues", methods=['GET','POST'])
+def menu_clues():
+    return render_template('menu_clues.html')
+
+#route for inventory tab in menu
+@app.route("/menu_inventory", methods=['GET', 'POST'])
+def menu_inventory():
+    return render_template('menu_inventory.html')
 
 
 #starts the website IMPORTANT don't touch
